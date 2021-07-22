49065dc205155c37cab2e467296679c81d9dd140
Will Bodron <wabo225@uky.edu>
2021-07-22 13:49:44 -0400

    longer top of fringe drift measurement

--- Data/oscilloscope/driftMeasurement.py
+++ Data/oscilloscope/driftMeasurement.py
@@ -3,7 +3,6 @@ import pyvisa
 import time
 import numpy as np
 from datetime import date
-import sys
 from Tektronics import Oscilloscope as Osc
 from toptica.lasersdk.dlcpro.v1_9_0 import DLCpro, SerialConnection
 # import asyncio
@@ -11,7 +10,7 @@ from toptica.lasersdk.dlcpro.v1_9_0 import DLCpro, SerialConnection
 def delta_frequency(delta_pixels, scale, expansion):
     expansions = {'1':467.2578713474481, '2':236.221920452312, '5':95.57796824314684}
     pixelsPerDivision = 250
-    return expansions['5']*float(delta_pixels)*float(scale)/pixelsPerDivision
+    return expansions[str(expansion)]*float(delta_pixels)*float(scale)/pixelsPerDivision
 
 def main():
     # Visa Connection Creation
@@ -20,7 +19,7 @@ def main():
     oscilloscope = rm.open_resource(rm.list_resources()[0]) # The Oscilloscope may not always be the first entry, but it has been for our USB Driver
     o = Osc.Oscilloscope(oscilloscope)
 
-    file = open('data\FrequencyDrift' +str(date.today())+'trial4.csv','w')  # filename
+    file = open('data\FrequencyDrift' +str(date.today())+'trial6.csv','w')  # filename
 
     expansion = Osc.findSweep(o)
     scale = o.HorizontalParams(Osc.HorizontalOptions.SCA)
@@ -30,10 +29,9 @@ def main():
     # print("Set Current, "+sys.argv[1]+'\n')
     # file.write("Power (mW), "+sys.argv[1]+'\n')
     # print("Power (mW), "+sys.argv[1]+'\n')
-    file.write('t (s), Frequency Drift (GHz), Active Current (mA) \n')
 
     # change these values 
-    duration = 15 # minutes
+    duration = 45 # minutes
     timeBetween = .25 # >= .1 min = 6 sec
     
     o.setChannel(4) # set channel for curv
@@ -48,11 +46,13 @@ def main():
         file.write('I,'+ str(dlc.laser1.dl.lock.pid2.gain.i.get())+'\n')
         file.write('D,'+ str(dlc.laser1.dl.lock.pid2.gain.d.get())+'\n')
         file.write('All,'+ str(dlc.laser1.dl.lock.pid2.gain.all.get())+'\n')
+        file.write('t (s), Frequency Drift (GHz), Active Current (mA), Piezo Voltage \n')
         for i in np.arange(0, duration, timeBetween):
             delta_pixels = np.argmax(o.CURV()) - x_init
             dnu = delta_frequency(delta_pixels, scale, expansion)
             act_curr = dlc.laser1.dl.cc.current_act.get()
-            out = str(round(time.time() - time0,3)) + ', ' + str(dnu) + ', ' + str(act_curr) + '\n'
+            piez_curr = dlc.laser1.dl.pc.voltage_act.get()
+            out = str(round(time.time() - time0,3)) + ', ' + str(dnu) + ', ' + str(act_curr) + ', ' + str(piez_curr) + '\n'
             print(out, end='')
             file.write(out)
             time.sleep(timeBetween*60)
